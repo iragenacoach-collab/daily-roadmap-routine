b294a0e98050d5fa2b2ca5ce7145b2e4f23417e9
iragenacoach-collab <iragenacoach@gmail.com>
2026-06-02 10:40:51 +0200

    Update fcm-config.js

--- fcm-config.js
+++ fcm-config.js
@@ -1,4 +1,4 @@
 // Firebase Cloud Messaging Web Push certificate key.
 // Get it from Firebase Console:
 // Project settings -> Cloud Messaging -> Web Push certificates -> Generate key pair
-export const FCM_VAPID_KEY = "PASTE_YOUR_WEB_PUSH_CERTIFICATE_KEY_HERE";
+export const FCM_VAPID_KEY = "BNSCnDtwQgzG0FzgilI_UHm79LCbCbe5gmvBZfCpTPzORN6f1zJJDCm4bY2yxKIn9Z-vksxzUhbRpAbn8M-80T8";
